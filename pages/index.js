e8b3b6fcf05eae18ba7f61f3bb32a498c5ffb7e2
Anzydev <112782934+zuckworld@users.noreply.github.com>
2025-07-24 08:26:34 +0100

    Updated Resume button

--- pages/index.js
+++ pages/index.js
@@ -42,7 +42,7 @@ export default function Home() {
               <li>
                 <a
                   className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8"
-                  href="#"
+                  href="/Anderson-Caleb-Software-Engineer.pdf"
                 >
                   Resume
                 </a>
